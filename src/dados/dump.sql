4a43b349cfbd1170ee6bd0abab0ae381ba89dec1
Isadora de Oliveira <isadoradeoliveirapinto@gmail.com>
2023-10-10 16:37:27 -0300

    Criacao do banco de dados

--- src/dados/dump.sql
+++ src/dados/dump.sql
@@ -1,1 +1,24 @@
---arquivo com a criação do banco de dados
+create table usuarios (
+id serial primary key,
+nome text not null,
+email text unique not null,
+senha text not null
+)
+
+create table categorias (
+id serial primary key,
+descricao text not null
+)
+
+insert into categorias (descricao)
+values
+('Informática'),
+('Celulares'),
+('Beleza e Perfumaria'),
+('Mercado'),
+('Livros e Papelaria'),
+('Brinquedos'),
+('Moda'),
+('Bebê'),
+('Games')
+
