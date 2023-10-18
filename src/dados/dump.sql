4d3b417cb1e32ba098d1d572effc4b9acf211ee3
Amanda Santos <amanda280721@gmail.com>
2023-10-18 14:30:06 -0400

    Tabelas de clientes e produtos criadas

--- src/dados/dump.sql
+++ src/dados/dump.sql
@@ -23,3 +23,23 @@ values
 ('Bebê'),
 ('Games')
 
+create table produtos (
+	id serial primary key,
+    descricao text not null,
+    quantidade_estoque integer,
+    valor integer,
+    categoria_id integer references categorias(id)
+);
+
+create table clientes (
+	id serial primary key,
+    nome text not null,
+    email text not null unique,
+    cpf char(11) not null unique,
+    cep text,
+    rua text,
+    numero integer,
+    bairro text,
+    cidade text,
+    estado char
+);
